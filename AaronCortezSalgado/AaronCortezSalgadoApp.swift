6bc7a68afd5fce98e8456fb6eaba1b3aad6243f3
AaronCSalgado <134568@swcsd.us>
2025-04-01 08:03:06 -0400

    Initial Commit

---
+++ AaronCortezSalgado/AaronCortezSalgadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AaronCortezSalgadoApp.swift
+//  AaronCortezSalgado
+//
+//  Created by AARON CORTEZ SALGADO on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AaronCortezSalgadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AaronCortezSalgadoTests/AaronCortezSalgadoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AaronCortezSalgadoTests.swift
+//  AaronCortezSalgadoTests
+//
+//  Created by AARON CORTEZ SALGADO on 4/1/25.
+//
+
+import Testing
+@testable import AaronCortezSalgado
+
+struct AaronCortezSalgadoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
